fa7caa9e6131716cb1317a88839f9a7baf86da72
saru-no-okiba <zaaakuro@gmail.com>
2025-04-10 08:49:45 +0900

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <osdVw/>
+</template>
+
+<script>
+import osdVw from './components/osdVw.vue'
+
+export default {
+  name: 'App',
+  components: {
+    osdVw
+  }
+}
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="hello">
+    aaaaa
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
